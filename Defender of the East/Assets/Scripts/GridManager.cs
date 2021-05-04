cb56b02c98356133ce9e679b00d4867f62c559c7
Md. Tanvir Siddiqui <babatanvir@hotmail.com>
2021-05-04 15:11:19 +0600

    Blocking Node

--- Defender of the East/Assets/Scripts/GridManager.cs
+++ Defender of the East/Assets/Scripts/GridManager.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class GridManager : MonoBehaviour
 {
     [SerializeField] Vector2Int gridSize;
+    [Tooltip("Unity Grid Size-should match UnityEditor snap settings.")]
+    [SerializeField] int unityGridSize=10;
     Dictionary<Vector2Int, Node> grid = new Dictionary<Vector2Int, Node>();
     public Dictionary<Vector2Int, Node> Grid { get { return grid; } }
+
+    public int UnityGridSize { get { return unityGridSize; } }
 
     void Awake()
     {
@@ -22,15 +26,40 @@ public class GridManager : MonoBehaviour
         }
         return null;
     }
+
+    public void BlockNode(Vector2Int coordinates) {
+        if (grid.ContainsKey(coordinates))
+        {
+            grid[coordinates].isWalkable = false;
+        }
+    } 
+
+    public Vector2Int GetcoordinatesFromPosition(Vector3 position)
+    {
+        Vector2Int coordinates = new Vector2Int();
+        coordinates.x = Mathf.RoundToInt(position.x / unityGridSize);
+        coordinates.y = Mathf.RoundToInt(position.z / unityGridSize);
+        return coordinates;
+    }
+
+    public Vector3 GetPositionFromCoordinates(Vector2Int coordinates)
+    {
+        Vector3 position = new Vector3();
+        position.x = coordinates.x * unityGridSize;
+        position.z = coordinates.y * unityGridSize;
+
+        return position;
+    }
     void CreateGrid()
     {
         for(int x=0; x < gridSize.x; x++)
         {
-            for(int y = 0; y < gridSize.y; y++)
+            for (int y = 0; y < gridSize.y; y++)
             {
                 Vector2Int coordinates = new Vector2Int(x, y);
                 grid.Add(coordinates, new Node(coordinates, true));
             }
+
         }
     }
 }
---
+++ Defender of the East/Assets/Scripts/Tile.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField] Tower towerPrefab;
+    [SerializeField] bool isPlacable;
+    Vector2Int coordinates = new Vector2Int();
+    
+    public bool IsPlacable{get{return isPlacable;}}  //property
+
+    GridManager gridManger;
+
+    void Awake()
+    {
+        gridManger = FindObjectOfType<GridManager>();    
+    }
+
+     void Start()
+    {
+        if (gridManger != null)
+        {
+            coordinates = gridManger.GetcoordinatesFromPosition(transform.position);
+            if (!isPlacable)
+            {
+                gridManger.BlockNode(coordinates);
+            }
+        }    
+    }
+
+    void OnMouseDown()
+    {
+       
+        if (isPlacable)
+        {
+            bool isPlaced = towerPrefab.CreateTower(towerPrefab,transform.position);
+            isPlacable =!isPlaced;
+        }
+    }
+}
